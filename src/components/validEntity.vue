80337b062c12bc829b9611ace40517c0a403cadb
makrozai <marco@combativa.com>
2019-06-24 18:18:07 -0500

    agrega validacion de administrador a entidades

--- src/components/validEntity.vue
+++ src/components/validEntity.vue
@@ -15,9 +15,13 @@
       </v-container>
     </v-card-title>
     <v-divider></v-divider>
-    <v-card-text>
+    <v-card-text class="c-valid-entity">
       <v-container grid-list-md >
         <v-layout wrap>
+          <v-flex xs12>
+            <h3>Información general</h3>
+            <p>la entidad requiere una validación de parte del administrador</p>
+          </v-flex>
           <v-flex xs8>
             <v-text-field
               disabled
@@ -25,35 +29,108 @@
               label="Razón social"
               box
             ></v-text-field>
+            <v-text-field
+              disabled
+              v-model="entity.line_id"
+              label="Tipo de rubro"
+              box
+            ></v-text-field>
           </v-flex>
           <v-flex xs4>
             <img :src="entity.logo_image_link || require('../assets/default-img.svg')" alt="">
           </v-flex>
+          <v-flex xs12>
+            <v-text-field
+              disabled
+              v-model="entity.type_entity_id"
+              label="Tipo de entidad"
+              box
+            ></v-text-field>
+
+          </v-flex>
+          <v-flex xs12>
+            <v-text-field
+              disabled
+              v-model="entity.website"
+              label="Sitio web"
+              box
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12>
+            <v-textarea
+                disabled
+                v-model="addressComplete"
+                label="Dirección"
+                box
+              ></v-textarea>
+          </v-flex>
+          <v-flex xs12>
+            <h3>Informacion de usuario</h3>
+            <p>informacion principal de la cuenta(no informacion de contacto)</p>
+          </v-flex>
+          <v-flex xs12>
+            <v-text-field
+              disabled
+              v-model="entity.main_phone"
+              label="Numero de teléfono"
+              box
+            ></v-text-field>
+          </v-flex>
         </v-layout>
       </v-container>
     </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-container class="py-0">
+        <v-btn
+          large
+          color="success"
+          @click="submit"
+          :disabled="entity.state == 1"
+        >
+          Validar
+        </v-btn>
+      </v-container>
+    </v-card-actions>
   </v-card>
 
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 export default {
   props: ['entity'],
   computed: {
-    ...mapState(['allEntities']),
+    ...mapState(['allEntities', 'ubigeo']),
     fullName () {
-      var namePlaceholder = 'RUC ' + this.entity.ruc + ' ' + this.entity.name
+      let namePlaceholder
+      if(this.entity.ruc && this. entity.name) {
+        namePlaceholder = 'RUC ' + this.entity.ruc + ' ' + this.entity.name
+      } else {
+        namePlaceholder = ''
+      }
+
       return namePlaceholder.toUpperCase()
+    },
+    addressComplete () {
+      let districtLabel = this.ubigeo.districts.filter(item => item.id === this.entity.districts_id)[0]
+      let provinceLabel = this.ubigeo.provinces.filter(item => item.id === this.entity.provinces_id)[0]
+      let regionLabel = this.ubigeo.regions.filter(item => item.id === this.entity.regions_id)[0]
+
+      return this.entity.address + " - " + districtLabel.name + ', ' + provinceLabel.name + ' | ' + regionLabel.name
     }
   },
-  /* created () {
-    console.log(this.entity)
-    console.log(this.allEntities.filter(item => item.id === this.entity))
-  }, */
-  data () {
-    return {
-      valueEntity: {}
+  methods: {
+    ...mapActions(['putEntity']),
+    submit () {
+      console.log('validado', this.entity.id)
+      this.putEntity({id: this.entity.id, state: 1})
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 }
