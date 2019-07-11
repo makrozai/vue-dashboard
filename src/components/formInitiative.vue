6cb848f64fecf6370cd84c592fbd413d9bf48f99
makrozai <marco@combativa.com>
2019-07-11 14:34:50 -0500

    fix input periodo  de select a number

--- src/components/formInitiative.vue
+++ src/components/formInitiative.vue
@@ -1,227 +1,227 @@
 <template>
-    <v-card class="c-form-program">
-      <v-card-text>
-        <v-container grid-list-md >
-          <v-layout wrap>
-            <h2>INICIATIVA</h2>
+  <v-card class="c-form-program">
+    <v-card-text>
+      <v-container grid-list-md >
+        <v-layout wrap>
+          <h2>INICIATIVA</h2>
 
-            <v-flex xs12 class="c-input__button-action">
-              <v-select
-                v-model="programSelected"
-                :items="allPrograms"
-                item-text="name"
-                item-value="id"
-                v-validate="'required'"
-                return-object
-                :error-messages="errors.collect('programa')"
-                label="Seleccionar el programa"
-                data-vv-name="programa"
-                class="pt-2"
-                required
-                box
-              >
-                <template v-slot:item="data">
-                  <v-list-tile-avatar>
-                    <img :src="data.item.logo_image_link || require('../assets/default-img.svg')">
-                  </v-list-tile-avatar>
-                  <v-list-tile-content>
-                    <v-list-tile-title v-html="data.item.name"></v-list-tile-title>
-                    <v-list-tile-sub-title v-html="data.item.type_program_name"></v-list-tile-sub-title>
-                  </v-list-tile-content>
-                </template>
-              </v-select>
-              <v-btn fab small color="primary">
-                <i class="icon-dots"></i>
-              </v-btn>
-            </v-flex>
+          <v-flex xs12 class="c-input__button-action">
+            <v-select
+              v-model="programSelected"
+              :items="allPrograms"
+              item-text="name"
+              item-value="id"
+              v-validate="'required'"
+              return-object
+              :error-messages="errors.collect('programa')"
+              label="Seleccionar el programa"
+              data-vv-name="programa"
+              class="pt-2"
+              required
+              box
+            >
+              <template v-slot:item="data">
+                <v-list-tile-avatar>
+                  <img :src="data.item.logo_image_link || require('../assets/default-img.svg')">
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="data.item.name"></v-list-tile-title>
+                  <v-list-tile-sub-title v-html="data.item.type_program_name"></v-list-tile-sub-title>
+                </v-list-tile-content>
+              </template>
+            </v-select>
+            <v-btn fab small color="primary">
+              <i class="icon-dots"></i>
+            </v-btn>
+          </v-flex>
 
-            <v-flex xs12>
-              <div class="c-card-program" v-if="programSelected">
-                <div class="c-card-program__image">
-                  <img :src="programSelected.logo_image_link || require('../assets/default-img.svg')" alt="">
-                  <div class="c-card-program__image__icon" v-if="programSelected.entities.length !== 0">
-                    <img  :src="programSelected.entities[0].logo_image_link || require('../assets/icons/user.svg')" alt="">
-                  </div>
+          <v-flex xs12>
+            <div class="c-card-program" v-if="programSelected">
+              <div class="c-card-program__image">
+                <img :src="programSelected.logo_image_link || require('../assets/default-img.svg')" alt="">
+                <div class="c-card-program__image__icon" v-if="programSelected.entities.length !== 0">
+                  <img  :src="programSelected.entities[0].logo_image_link || require('../assets/icons/user.svg')" alt="">
                 </div>
-                <p class="c-card-project__description">
-                  {{ programSelected.description }}
-                </p>
               </div>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                v-model="initiativeData.name"
-                v-validate="'required'"
-                :error-messages="errors.collect('nombre de iniciativa')"
-                label="Nombre de iniciativa"
-                data-vv-name="nombre de iniciativa"
-                required
-                box
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs6>
-              <v-dialog
-                ref="dialogDate"
-                v-model="dateStartModal"
-                :return-value.sync="initiativeData.start_date"
-                lazy
-                full-width
-                width="290px"
-              >
-                <template v-slot:activator="{ on }">
-                  <v-text-field
-                    v-model="initiativeData.start_date"
-                    v-validate="'required'"
-                    :error-messages="errors.collect('fecha de inicio')"
-                    label="Fecha de inicio"
-                    data-vv-name="fecha de inicio"
-                    @keyup.enter="submit"
-                    required
-                    readonly
-                    box
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  ref="picker"
-                  v-model="initiativeData.start_date"
-                  type="month"
-                  locale="es"
-                  :max="new Date().toISOString().substr(0, 10)"
-                  scrollable
-                >
-                  <v-spacer></v-spacer>
-                  <v-btn flat color="primary" @click="dateStartModal = false">Cancel</v-btn>
-                  <v-btn flat color="primary" @click="$refs.dialogDate.save(initiativeData.start_date)">OK</v-btn>
-                </v-date-picker>
-              </v-dialog>
-            </v-flex>
-            <v-flex xs6>
-              <v-select
-                v-model="initiativeData.intervention_period"
-                :items="typeLines"
-                item-text="name"
-                item-value="id"
-                v-validate="'required'"
-                :error-messages="errors.collect('periodo de intervención')"
-                label="Periodo de intervención"
-                data-vv-name="periodo de intervención"
-                required
-                box
-              ></v-select>
-            </v-flex>
-            <v-flex xs12 class="mb-2">
-              <h3>Entidad(es) comprometida(s)</h3>
-            </v-flex>
-            <!--autocompletado-->
-            <v-flex xs12 class="c-input__button-action">
-              <v-combobox
-                v-model="entitySelect"
-                :items="allEntities"
-                label="Buscar entidad comprometida"
-                item-text="name"
-                item-value="id"
-                prepend-inner-icon="search"
-                box
-              >
-                <template v-slot:item="data">
-                  <v-list-tile-avatar>
-                    <img :src="data.item.logo_image_link || require('../assets/default-img.svg')">
-                  </v-list-tile-avatar>
-                  <v-list-tile-content>
-                    <v-list-tile-title v-html="data.item.name"></v-list-tile-title>
-                    <v-list-tile-sub-title v-html="data.item.ruc"></v-list-tile-sub-title>
-                  </v-list-tile-content>
-                </template>
-              </v-combobox>
-              <v-btn fab small color="primary" class="mt-2" @click="registerInvolved">
-                <v-icon>add</v-icon>
-              </v-btn>
-
-            </v-flex>
+              <p class="c-card-project__description">
+                {{ programSelected.description }}
+              </p>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-text-field
+              v-model="initiativeData.name"
+              v-validate="'required'"
+              :error-messages="errors.collect('nombre de iniciativa')"
+              label="Nombre de iniciativa"
+              data-vv-name="nombre de iniciativa"
+              required
+              box
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs4>
             <v-dialog
-              v-model="addInvolveds"
+              ref="dialogDate"
+              v-model="dateStartModal"
+              :return-value.sync="initiativeData.start_date"
+              lazy
+              full-width
+              width="290px"
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  v-model="initiativeData.start_date"
+                  v-validate="'required'"
+                  :error-messages="errors.collect('fecha de inicio')"
+                  label="Fecha de inicio"
+                  data-vv-name="fecha de inicio"
+                  @keyup.enter="submit"
+                  required
+                  readonly
+                  box
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                ref="picker"
+                v-model="initiativeData.start_date"
+                type="month"
+                locale="es"
+                :max="new Date().toISOString().substr(0, 10)"
+                scrollable
+              >
+                <v-spacer></v-spacer>
+                <v-btn flat color="primary" @click="dateStartModal = false">Cancel</v-btn>
+                <v-btn flat color="primary" @click="$refs.dialogDate.save(initiativeData.start_date)">OK</v-btn>
+              </v-date-picker>
+            </v-dialog>
+          </v-flex>
+          <v-flex xs4>
+            <v-text-field
+              v-model="initiativeData.intervention_period"
+              v-validate="'required|integer|max:4'"
+              :error-messages="errors.collect('periodo')"
+              label="Periodo"
+              data-vv-name="periodo"
+              required
+              box
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs4>
+            <p class="mt-3 c-period__text">meses de actividad</p>
+          </v-flex>
+          <v-flex xs12 class="mb-2">
+            <h3>Entidad(es) comprometida(s)</h3>
+          </v-flex>
+          <!--autocompletado-->
+          <v-flex xs12 class="c-input__button-action">
+            <v-combobox
+              v-model="entitySelect"
+              :items="allEntities"
+              label="Buscar entidad comprometida"
+              item-text="name"
+              item-value="id"
+              prepend-inner-icon="search"
+              box
+            >
+              <template v-slot:item="data">
+                <v-list-tile-avatar>
+                  <img :src="data.item.logo_image_link || require('../assets/default-img.svg')">
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="data.item.name"></v-list-tile-title>
+                  <v-list-tile-sub-title v-html="data.item.ruc"></v-list-tile-sub-title>
+                </v-list-tile-content>
+              </template>
+            </v-combobox>
+            <v-btn fab small color="primary" class="mt-2" @click="registerInvolved">
+              <v-icon>add</v-icon>
+            </v-btn>
+
+          </v-flex>
+          <v-dialog
+            v-model="addInvolveds"
+            lazy
+            full-width
+            width="500px"
+          >
+            <form-involved :entity="openEntityInvoled" :change-value="checkOtherEntity" @involed="AddInvoled" @modal-close="closeInvoled"></form-involved>
+          </v-dialog>
+
+          <!--autocompletado-->
+          <v-flex xs12>
+            <card-entity :entities="entitiesParticipans"></card-entity>
+          </v-flex>
+          <v-flex xs12 class="mb-2">
+            <h3>Beneficiados</h3>
+          </v-flex>
+          <!--autocompletado-->
+          <v-flex xs12 class="c-input__button-action">
+            <v-combobox
+              v-model="benefitSelect"
+              :items="allBeneficiaries"
+              label="Buscar entidad propietaria del programa"
+              item-text="name"
+              item-value="id"
+              prepend-inner-icon="search"
+              return-object
+              box
+            >
+            </v-combobox>
+            <v-btn fab small color="primary" class="mt-2" @click="registerBeneficiare">
+              <v-icon>add</v-icon>
+            </v-btn>
+
+            <v-dialog
+              v-model="addBeneficiaries"
               lazy
               full-width
               width="500px"
             >
-              <form-involved :entity="openEntityInvoled" :change-value="checkOtherEntity" @involed="AddInvoled" @modal-close="closeInvoled"></form-involved>
+              <form-beneficiaries @beneficiary="addBeneficiary" @modal-close="closeBeneficiaries"></form-beneficiaries>
             </v-dialog>
 
-            <!--autocompletado-->
-            <v-flex xs12>
-              <card-entity :entities="entitiesParticipans"></card-entity>
-            </v-flex>
-            <v-flex xs12 class="mb-2">
-              <h3>Beneficiados</h3>
-            </v-flex>
-            <!--autocompletado-->
-            <v-flex xs12 class="c-input__button-action">
-              <v-combobox
-                v-model="benefitSelect"
-                :items="allBeneficiaries"
-                label="Buscar entidad propietaria del programa"
-                item-text="name"
-                item-value="id"
-                prepend-inner-icon="search"
-                return-object
-                box
-              >
-              </v-combobox>
-              <v-btn fab small color="primary" class="mt-2" @click="registerBeneficiare">
-                <v-icon>add</v-icon>
-              </v-btn>
+            <v-dialog
+              v-model="editBeneficiaryDialog"
+              lazy
+              full-width
+              width="500px"
+            >
+              <form-beneficiaries-edit :entity="editBeneficiaryData" @beneficiary="editBeneficiaryUpdate" @modal-close="closeEditBeneficiaries"></form-beneficiaries-edit>
+            </v-dialog>
+          </v-flex>
+          <!--autocompletado-->
+          <v-flex xs12 class="mb-4">
+            <card-benefit :entities="benefitiesParticipans" @delete="deleteBeneficiary" @edit="editBeneficiary"></card-benefit>
+          </v-flex>
+          <v-flex xs7>
+            <p>Indicar el monto en soles de la inversión generada en la iniciativa</p>
+          </v-flex>
+          <v-flex xs5>
+            <v-text-field
+              v-model="initiativeData.total_investment_amount"
+              v-validate="'required'"
+              :error-messages="errors.collect('Inversión')"
+              label="Inversión"
+              data-vv-name="Inversión"
+              required
+              box
+            ></v-text-field>
+          </v-flex>
 
-              <v-dialog
-                v-model="addBeneficiaries"
-                lazy
-                full-width
-                width="500px"
-              >
-                <form-beneficiaries @beneficiary="addBeneficiary" @modal-close="closeBeneficiaries"></form-beneficiaries>
-              </v-dialog>
+        </v-layout>
+      </v-container>
+    </v-card-text>
 
-              <v-dialog
-                v-model="editBeneficiaryDialog"
-                lazy
-                full-width
-                width="500px"
-              >
-                <form-beneficiaries-edit :entity="editBeneficiaryData" @beneficiary="editBeneficiaryUpdate" @modal-close="closeEditBeneficiaries"></form-beneficiaries-edit>
-              </v-dialog>
-            </v-flex>
-            <!--autocompletado-->
-            <v-flex xs12 class="mb-4">
-              <card-benefit :entities="benefitiesParticipans" @delete="deleteBeneficiary" @edit="editBeneficiary"></card-benefit>
-            </v-flex>
-            <v-flex xs7>
-              <p>Indicar el monto en soles de la inversión generada en la iniciativa</p>
-            </v-flex>
-            <v-flex xs5>
-              <v-text-field
-                v-model="initiativeData.total_investment_amount"
-                v-validate="'required'"
-                :error-messages="errors.collect('Inversión')"
-                label="Inversión"
-                data-vv-name="Inversión"
-                required
-                box
-              ></v-text-field>
-            </v-flex>
+    <v-divider></v-divider>
 
-          </v-layout>
-        </v-container>
-      </v-card-text>
+    <v-card-actions>
+      <v-container class="py-0">
+        <v-btn large color="primary" @click="submit">Guardar</v-btn>
+      </v-container>
 
-      <v-divider></v-divider>
-
-      <v-card-actions>
-        <v-container class="py-0">
-          <v-btn large color="primary" @click="submit">Guardar</v-btn>
-        </v-container>
-
-      </v-card-actions>
-    </v-card>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -264,70 +264,7 @@ export default {
       entitySelect: null,
       benefitSelect: null,
       entitiesParticipans: [],
-      benefitiesParticipans: [],
-      // eslint-disable-next-line
-      typeLines: [
-        {
-          id: 1,
-          name: '1 mes'
-        },
-        {
-          id: 2,
-          name: '2 meses'
-        },
-        {
-          id: 3,
-          name: '3 meses'
-        },
-        {
-          id: 4,
-          name: '4 meses'
-        },
-        {
-          id: 5,
-          name: '5 meses'
-        },
-        {
-          id: 5,
-          name: '5 meses'
-        },
-        {
-          id: 6,
-          name: '6 meses'
-        },
-        {
-          id: 7,
-          name: '7 meses'
-        },
-        {
-          id: 8,
-          name: '8 meses'
-        },
-        {
-          id: 9,
-          name: '9 meses'
-        },
-        {
-          id: 10,
-          name: '10 meses'
-        },
-        {
-          id: 11,
-          name: '11 meses'
-        },
-        {
-          id: 12,
-          name: '12 meses'
-        },
-        {
-          id: 13,
-          name: '13 meses'
-        },
-        {
-          id: 14,
-          name: '14 meses'
-        }
-      ]
+      benefitiesParticipans: []
     }
   },
   watch: {
